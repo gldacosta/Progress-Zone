11741c9170e6441f2c18ce01ed7225b1db4dbc6a
gldacosta <gelmar.costa@acad.ucrs.br>
2021-02-11 10:23:12 -0300

    Python

---
+++ Python/Lists/List.py
@@ -0,0 +1,17 @@
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+mylist.append(4)
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+print(mylist[3]) # prints 4
+
+# prints out 1,2,3,4
+for x in mylist:
+	print(x)
+
+
+#mylist = [1,2,3]
+#print(mylist[0])
