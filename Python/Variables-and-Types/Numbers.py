5b3287561ea25d7b6be46b90ab3396403d0e0091
gldacosta <gelmar.costa@acad.ucrs.br>
2021-02-11 10:11:36 -0300

    Python

---
+++ Python/Variables-and-Types/Numbers.py
@@ -0,0 +1,6 @@
+myint = 8
+print(myint)
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
---
+++ Python/Variables-and-Types/Strings.py
@@ -0,0 +1,20 @@
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
+mystring = "Dont't worry about apostrophes"
+print(mystring)
+
+
+one = 1
+two = 2
+theree = one + two
+print(theree)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+a,b = 3, 4
+print(a,b)
