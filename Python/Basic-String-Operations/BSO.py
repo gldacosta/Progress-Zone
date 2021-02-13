1ea77347c76244a1e98a9b322c1a8a70985ba4e1
gldacosta <gelmar.costa@acad.ucrs.br>
2021-02-13 14:22:46 -0300

    Python

---
+++ Python/Basic-String-Operations/BSO.py
@@ -0,0 +1,16 @@
+astring = "Hello World!"
+#print("single quotes are ' '")
+
+#print(len(astring))
+#print(astring.index("o"))
+#print(astring.count("l"))
+#print(astring[3:7]) #[start:stop]
+#print(astring[3:7:2]) #[start:stop:step]
+#print(astring[3:7])
+#print(astring[3:7:1])
+#print(astring[::-1]) #inverter string
+#print(astring.upper()) #tudo maiusculo
+#print(astring.lower()) #tudo minusculo
+#print(astring.startswith("Hello"))
+#print(astring.endswith("asdfasdf"))
+afewwords = astring.split(" ")
