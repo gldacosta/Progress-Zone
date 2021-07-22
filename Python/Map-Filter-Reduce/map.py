5f0979b38529ef3480ab54ac000dbfcc7d6375c1
gldacosta <gelmar.costa@acad.ucrs.br>
2021-07-22 13:22:42 -0300

    Python

---
+++ Python/Map-Filter-Reduce/map.py
@@ -0,0 +1,40 @@
+my_pets = ['alfred', 'tabitha', 'william', 'arla']
+uppered_pets = []
+
+for pet in my_pets:
+    pet_ = pet.upper()
+    uppered_pets.append(pet_)
+
+print(uppered_pets)
+
+# Python 3
+my_pets
+
+uppered_pets = list(map(str.upper, my_pets))
+
+print(uppered_pets)
+
+# Python 3
+
+circle_areas = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.00013]
+
+result = list(map(round, circle_areas, range(1,7)))
+
+print(result)
+
+# Python 3
+
+circle_areas = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.00013]
+
+result = list(map(round, circle_areas, range(1,3)))
+
+print(result)
+
+# Python 3
+
+my_strings = ['a', 'b', 'c', 'd', 'e']
+my_numbers = [1,2,3,4,5]
+
+results = list(zip(my_strings, my_numbers))
+
+print(results)
