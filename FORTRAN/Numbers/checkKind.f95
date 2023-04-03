6c3a5a187050b2ee04b442e3c6369e5e6894d79a
Gelmar Luiz DA COSTA <gelmar.costa@acad.pucrs.br>
2023-04-03 16:37:50 +0200

    f95

---
+++ FORTRAN/Numbers/checkKind.f95
@@ -0,0 +1,16 @@
+program checkKind
+implicit none
+
+   integer :: i 
+   real :: r 
+   character :: c 
+   logical :: lg 
+   complex :: cp 
+   
+   print *,' Integer ', kind(i) 
+   print *,' Real ', kind(r) 
+   print *,' Complex ', kind(cp)
+   print *,' Character ', kind(c) 
+   print *,' Logical ', kind(lg)
+   
+end program checkKind
